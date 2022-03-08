89b448085b1e83bfe5e40bffc4df9b0d7f8aa1b7
lafonth <lafonthub@gmail.com>
2022-03-08 15:49:38 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import firebase from "firebase/compat/app";
+// import { getAnalytics } from "firebase/analytics";
+import "firebase/compat/firestore";
+
+import {useCollectionData} from "react-firebase-hooks/firestore";
+
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
+import PhoneIcon from '@mui/icons-material/Phone';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDrzpgW8PkB8JbWEZr3XSeH6TBQNUMdyWs",
+  authDomain: "web-cv-hubert-lafont.firebaseapp.com",
+  projectId: "web-cv-hubert-lafont",
+  storageBucket: "web-cv-hubert-lafont.appspot.com",
+  messagingSenderId: "997976969094",
+  appId: "1:997976969094:web:790b57d26b040a999d6607",
+  measurementId: "G-9ZNC63QEPY"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+const db = firebase.firestore();
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Contacts></Contacts>
       </header>
     </div>
+  );
+}
+
+function Contacts(){
+  const contactsRef = db.collection('contact');
+  const [contacts, loadingContacts] = useCollectionData(contactsRef, {
+    idField: "id"
+  });
+  return (
+    <>
+    <h1>CONTACT</h1>
+      {contacts && contacts.map(contact => <Contact key={contact.id} contact={contact} />)} 
+    </>
+  )
+}
+
+function Contact(props){
+  const contact = props.contact;
+  
+  return (
+    <p>
+      {
+        (() => {
+          switch (contact.label) {
+            case "Email":
+              return <AlternateEmailIcon fontSize='medium'/>;
+            case "Name":
+              return <AccountBoxIcon fontSize='medium'/>;
+            case "Phone":
+              return <PhoneIcon fontSize='medium'/>;
+            case "GitHub":
+              return <GitHubIcon fontSize='medium'/>;
+            case "LinkedIn":
+              return <LinkedInIcon fontSize='medium'/>;
+            default:
+              return null;
+          }
+        })()
+      }
+      {contact.value}
+    </p>
   );
 }
 
